90bbfe1f41348053c7efb995f728e374a9e44c94
Code Nurturers LLC <mobileapps@codenurturers.com>
2024-09-15 19:00:26 -0500

    interview

---
+++ interview.py
@@ -0,0 +1,11 @@
+# Define the Robot class
+class Robot:
+    pass  # You need to implement this
+
+# Define the function to find the fastest robot
+def find_fastest_robot(robots):
+    pass  # You need to implement this
+
+# Create robot instances and test your code
+# Example: robot1 = Robot(distance=10, speed=2)
+
